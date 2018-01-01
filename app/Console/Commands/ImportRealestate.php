d885d665938847052cae6bd576c4659c1efb047c
Nirys <kath2chapman@gmail.com>
2018-01-02 06:00:21 +1030

    Importing commanding.

--- app/Console/Commands/ImportRealestate.php
+++ app/Console/Commands/ImportRealestate.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class ImportRealestate extends Command
 {
@@ -30,14 +32,62 @@ class ImportRealestate extends Command
         parent::__construct();
     }
 
+    protected function getResultUrl($url){
+        $client = new Client(); //GuzzleHttp\Client
+        $result = $client->get($url);
+
+        $data = ($result->getBody()->getContents());
+        return $this->parseREPage($data);
+    }
+
+    protected function parseREPage($data){
+        preg_match_all("/<script[\s\S]*?>([\s\S]*?)<\/script>/", $data, $matches);
+        $results = array();
+        foreach($matches[1] as $match){
+            if( strpos($match, "REA.resultsData =") !== false ) {
+                $item = substr($match, strpos($match, "REA.resultsData =")+18, strlen($match));
+                $results = json_decode($item, true);
+            }
+        }
+        return $results;
+    }
+
+    protected function getResultData(){
+        $totalResults = [];
+
+        $url = "https://m.realestate.com.au/buy/with-4-bedrooms-between-0-650000-in-";
+        $suburbs = config('realestate.suburbs');
+        foreach($suburbs as $suburb){
+            $url .= urlencode($suburb . ";");
+        }
+        $url .= "/list-1?activeSort=list-date&adcall=1514348785943";
+
+        echo "Get $url \n";
+        $data = $this->parseREPage( file_get_contents("dummy_data.html") ); //$this->getResultUrl($url);
+        $total = $data['totalResultsCount'];
+        $currentPage = $data['resolvedQuery']['page'];
+        $pageSize = $data['resolvedQuery']['pageSize'];
+
+        echo "On page $currentPage of $total results of $pageSize each page\n";
+        return;
+        $BreakOut = 20;
+        while(isset($data['_links']) && isset($data['_links']['next'])){
+            $nextLink = $data['_links']['next']['href'];
+            echo "Get " . $nextLink . "\n";
+            $data = $this->getResultUrl($nextLink);
+            sleep(1);
+            $BreakOut++;
+        }
+    }
+
     /**
      * Execute the console command.
      *
      * @return mixed
      */
-    public function handle()
-    {
-        //
+    public function handle() {
+        $data = $this->getResultData();
 
+        echo "Done\n";
     }
 }
---
+++ config/realestate.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kath
+ * Date: 31/12/2017
+ * Time: 1:58 PM
+ */
+
+return [
+    'suburbs' => [
+        'Woodcroft, SA 5162',
+        'Bellevue Heights, SA 5050',
+        'Happy Valley, SA 5159',
+        'Eden Hills, SA 5050',
+        'Blackwood, SA 5051',
+        'Coromandel Valley, SA 5051',
+        'Flagstaff Hill, SA 5159'
+    ]
+];
